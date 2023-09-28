1a03a46a3b9679cd9f5fad060dba0dc29bf43664
afruiz01 <141862421+afruiz01@users.noreply.github.com>
2023-09-28 14:41:57 -0500

    Add files via upload
    constructores tienda mascostas

--- PROYECTORUIZ/demo/src/Tienda.java
+++ PROYECTORUIZ/demo/src/Tienda.java
@@ -1,7 +1,25 @@
-public enum Tienda{
-    NOMBRE,
-    DIRECCION,
-}
-public class Tienda {
+public class tienda {
+    private String nombre;
+    private String direccion = "";
+
+
+    Public Tienda(String nombre, String direccion){
+        super();
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        nombre = nombre;
+    }
+    public String getDireccion(){
+        return direccion:
+    }
+    public void setDireccion(String direccion){
+        direccion = direccion;
+    }
     
 }
